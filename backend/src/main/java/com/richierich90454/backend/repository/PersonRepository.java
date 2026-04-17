9097f2923a32860e6eb3b81cd101e93c9c791c34
richie-rich90454 <rjiang880@gmail.com>
2026-04-18 07:38:13 +0800

    feat: add PersonRepository with findByCivilizationId method
    - Create PersonRepository interface extending JpaRepository
    - Add custom query method to find persons by civilizationId
    - Enable data access layer for Person entities with civilization filtering

---
+++ backend/src/main/java/com/richierich90454/backend/repository/PersonRepository.java
@@ -0,0 +1,9 @@
+package com.richierich90454.backend.repository;
+
+import com.richierich90454.backend.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface PersonRepository extends JpaRepository<Person, Long>{
+    List<Person> findByCivilizationId(Long civilizationId);
+}
