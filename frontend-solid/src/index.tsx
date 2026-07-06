d897dce736a705ad1cd9773e47f87a62e66d45e8
richie-rich90454 <rjiang880@gmail.com>
2026-07-06 20:27:30 +0800

    style: add global reset

--- frontend-solid/src/index.tsx
+++ frontend-solid/src/index.tsx
@@ -1,6 +1,7 @@
 /* @refresh reload */
 import { render } from 'solid-js/web'
 import './index.css'
+import "./styles/global.css";
 import App from './App.tsx'
 
 const root = document.getElementById('root')
